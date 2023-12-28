9b0c4a1a496723318115f927aae1a2b584e7d196
SrikanthNamoju5677 <135813675+SrikanthNamoju5677@users.noreply.github.com>
2023-12-28 10:43:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,5 +4,17 @@
 import shutil 
   
 # use copyfile() 
-shutil.copyfile('example.txt','example2.txt')
+# shutil.copyfile('example.txt','example2.txt')
 
+file_path = 'example.txt'
+line_number_tomodify = 3
+newcontent = 'file got changed'
+
+with open(file_path, 'r') as file:
+  lines = file.readlines()
+
+if 1 <= line_number_tomodify <= len(lines):
+  lines[line_number_tomodify - 1] = newcontent + '\n'
+
+with open(file_path, 'r') as file:
+  lines = file.writelines(lines)
