06e60f0733dfbfc03d761bb8d82e49cb454387ae
SrikanthNamoju5677 <135813675+SrikanthNamoju5677@users.noreply.github.com>
2023-12-28 17:57:47 +0100

    Update main1.py

--- main1.py
+++ main1.py
@@ -3,10 +3,21 @@
 
 import shutil, os
 
-def append_after_line('example.txt', 'example2.txt', 10):
-  with open('example.txt', 'r') as file1:
-    contenttoappend = file1.read()
-   with open('example2.txt', "a") as file2:
-     lines = file2.readlines()
-     start_line = min(10,len(lines) + 1)
-     file2.write(contenttoappend)
+# def append_after_line('example.txt', 'example2.txt', 10):
+#   with open('example.txt', 'r') as file1:
+#     contenttoappend = file1.read()
+#    with open('example2.txt', "a") as file2:
+#      lines = file2.readlines()
+#      start_line = min(10,len(lines) + 1)
+#      file2.write(contenttoappend)
+fp = open("example.txt","r")
+# fp is file1
+data = fp.read()
+f = open('example2.txt', 'rw+')
+#f is file2 on which you want to write data
+ss = f.readlines()
+f.seek(0)
+f.truncate()
+ss.insert(5,data)
+#give your own line number instead of 5
+f.write(''.join(ss))
