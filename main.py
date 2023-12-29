555b7cddf7d235cb2db60307b59c013689118494
SrikanthNamoju5677 <135813675+SrikanthNamoju5677@users.noreply.github.com>
2023-12-29 11:48:11 +0100

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ if 1 <= line_to_read <= len(lines):
   desired_line = lines[line_to_read - 1].strip()
   variable = desired_line[80:86]
   print(variable)
-  data = data.replace(variable, "replaced") )
+  data = data.replace(variable, "replaced")
   with open(file_path, 'w') as file:
     file.write(data)
   print("Text replaced")
