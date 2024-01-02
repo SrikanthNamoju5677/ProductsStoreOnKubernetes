92012f61046c25b2806aee746fc6a9c503ad5135
SrikanthNamoju5677 <135813675+SrikanthNamoju5677@users.noreply.github.com>
2024-01-02 16:56:20 +0100

    Update main1.py

--- main1.py
+++ main1.py
@@ -51,4 +51,37 @@ if 1 <= line_to_write <= len(line):
 else:
   print("invalid line")
   
+def replace_line("example2.txt","7", data2):
+
+# Open the file and read all the lines from the file into a list 'lines'
+with open('example2.txt') as file:
+  lines = file.readlines()
+
+# if the line number is in the file, we can replace it successfully
+if (7 <= len(lines)):
+  
+  # Replace the associated line in the list with the replacement text 
+  # (followed by a newline \n to end the line), we need to use line_number - 1
+  # as the index because lists are zero-indexed in Python.
+  lines[line_number - 1] = data2 + "\n"
+  
+  # Open the file in 'writing mode' using the 2nd argument "w", this means 
+  # that the file will be made blank, and any new text we write to the file 
+  # will become the new file contents.
+  with open('example2.txt', "w") as file:
+
+    # Loop through the list of lines, write each of them to the file
+    for line in lines:
+      file.write(line)
+
+# otherwise if the line number is past the length of the file, we can't 
+# replace the line so output an error message instead
+else:
+
+  # Output the line number that was requested to be replaced and the number
+  # of lines the file actually has to inform the user
+  print("Line", 7, "not in file.")
+  print("File has", len(lines), "lines.")
+
+  
   
