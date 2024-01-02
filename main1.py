0be6b2f7d8f90c5f3838d06e54457f11bef260c7
SrikanthNamoju5677 <135813675+SrikanthNamoju5677@users.noreply.github.com>
2024-01-02 15:44:14 +0100

    Update main1.py

--- main1.py
+++ main1.py
@@ -1,31 +1,46 @@
 #!/apps/sss/Anaconda3/5.0.0/bin/python
 # -*- coding: utf-8 -*-
 
-import shutil, os
+import shutil 
+  
+# use copyfile() 
+# shutil.copyfile('example.txt','example2.txt')
 
-# def append_after_line('example.txt', 'example2.txt', 10):
-#   with open('example.txt', 'r') as file1:
-#     contenttoappend = file1.read()
-#    with open('example2.txt', "a") as file2:
-#      lines = file2.readlines()
-#      start_line = min(10,len(lines) + 1)
-#      file2.write(contenttoappend)
-# fp = open("example.txt","r")
-# # fp is file1
-# data = fp.read()
-# f = open("example2.txt", "r")
-# #f is file2 on which you want to write data
-# ss = f.readlines()
-# f.seek(0)
-# f.truncate()
-# ss.insert(5,data)
-# #give your own line number instead of 5
-# f.write(''.join(ss))
+# file_path = 'example2.txt'
+# line_to_read = 2
+# specified2_linenumber = 10
 
-with open("example.txt", "r") as f1:
-    t1 = f1.readlines()
-with open("example2.txt", "r") as f2:
-    t2 = f2.readlines()
-t2.insert(20, t1)
-with open("example2.txt", "w") as f2:
-    f2.writelines(t2)
+
+
+# op = open('example2.txt', 'r')
+# lines = op.readlines()
+# print(lines)
+# ip = open('example.txt', "r")
+# content_to_append = ip.read()
+
+# op = open('example2.txt', 'w')
+# op.writelines(lines[:specified2_linenumber])
+# # data = ip.writelines(lines[:specified2_linenumber])
+# # print(data)
+# # appendfile = open('example.txt', "r")
+# # data2 = appendfile.read()
+# # print(data2)
+# op.write(content_to_append)
+# op.writelines(lines[specified2_linenumber:])
+# op.close()
+
+
+with open(file_path, 'r') as file:
+  lines = file.readlines()
+  # data = file.read(line_to_read)
+
+# if 1 <= line_to_read <= len(lines):
+  desired_line = lines[line_to_read - 1].strip()
+  variable = desired_line[80:86]
+  print(variable)
+  data2 = desired_line.replace(variable, "replaced")
+  print(data2)
+  print("Text replaced")
+  # lines = file.writelines(line.replace('variable', '7777777'))
+# else:
+  print("invalid line")
