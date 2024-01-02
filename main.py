e2cfd630036e6a5a20d2454bc660cefc70155a17
SrikanthNamoju5677 <135813675+SrikanthNamoju5677@users.noreply.github.com>
2024-01-02 13:45:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import shutil
 # use copyfile() 
 # shutil.copyfile('example.txt','example2.txt')
 
-file_path = 'example.txt'
+file_path = 'example2.txt'
 line_to_read = 2
 specified2_linenumber = 10
 
@@ -25,20 +25,20 @@ f1.close()
 f2.close()
 
 
-# with open(file_path, 'r') as file:
-#   lines = file.readlines()
-#   data = file.read(line_to_read)
+with open(file_path, 'r') as file:
+  lines = file.readlines()
+  data = file.read(line_to_read)
 
-# if 1 <= line_to_read <= len(lines):
-#   desired_line = lines[line_to_read - 1].strip()
-#   variable = desired_line[80:86]
-#   print(variable)
-#   data2 = desired_line.replace(variable, "replaced")
-#   print(data2)
-#   with open(file_path, 'w') as file:
-#     file.write(data2)
-#   print("Text replaced")
-#   # lines = file.writelines(line.replace('variable', '7777777'))
-# else:
-#   print("invalid line")
+if 1 <= line_to_read <= len(lines):
+  desired_line = lines[line_to_read - 1].strip()
+  variable = desired_line[80:86]
+  print(variable)
+  data2 = desired_line.replace(variable, "replaced")
+  print(data2)
+  with open(file_path, 'w') as file:
+    file.write(data2)
+  print("Text replaced")
+  # lines = file.writelines(line.replace('variable', '7777777'))
+else:
+  print("invalid line")
 
