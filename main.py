d0de67a71296de22b190bc118a4d1a40284f19e2
SrikanthNamoju5677 <135813675+SrikanthNamoju5677@users.noreply.github.com>
2024-01-02 12:23:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -12,10 +12,10 @@ specified2_linenumber = 10
 
 with open('example.txt', 'r') as file1:
   lines = file1.readlines()
-  print(file1)
+  print(lines)
 with open('example2.txt', "w") as file2:
-   file2.writelines(lines[:specified2_linenumber])
-print(file2)
+   data = file2.writelines(lines[:specified2_linenumber])
+print(data)
 with open('example.txt', "r") as appendfile:
   print(appendfile)
    file2.write(appendfile.read())
