509ea9a6a4ca6b138a82a2358612f0ab1e3deb3a
SrikanthNamoju5677 <135813675+SrikanthNamoju5677@users.noreply.github.com>
2024-01-03 11:03:50 +0100

    Update main1.py

--- main1.py
+++ main1.py
@@ -6,59 +6,50 @@ import shutil
 # use copyfile() 
 # shutil.copyfile('example.txt','example2.txt')
 
-file_path = 'example.txt'
+release_notes = 'example.txt'
+release_history = 'example2.txt'
+release_template = 'example1.txt'
 line_to_read = 2
 line_to_write = 7
-specified2_linenumber = 15
+specific_line_number = 14
 
 
+output_file = open(release_history, 'r')
+lines = output_file.readlines()
+input_file = open(release_notes, "r")
+content_to_append = input_file.read()
 
-op = open('example2.txt', 'r')
-lines = op.readlines()
-print(lines)
-ip = open('example.txt', "r")
-content_to_append = ip.read()
-
-op = open('example2.txt', 'w')
-op.writelines(lines[:specified2_linenumber])
-# data = ip.writelines(lines[:specified2_linenumber])
-# print(data)
-# appendfile = open('example.txt', "r")
-# data2 = appendfile.read()
-# print(data2)
-op.write(content_to_append)
-op.writelines(lines[specified2_linenumber:])
-op.close()
+output_file = open(release_history, 'r')
+output_file.writelines(lines[:specific_line_number])
+output_file.write(content_to_append)
+output_file.writelines(lines[specific_line_number:])
+output_file.close()
 
 
-with open(file_path, 'r') as file1:
+with open(release_notes, 'r') as file1:
   lines = file1.readlines()
   desired_line = lines[line_to_read - 1].strip()
   updateversion = desired_line[80:86]
   print(updateversion)
-with open('example2.txt', 'r') as file2:
+with open(release_history, 'r') as file2:
     line = file2.readlines()  
   
 
 if 1 <= line_to_write <= len(line):
     desired_lines = line[line_to_write - 1].strip()
-    variable = desired_lines[42:48]
-    print(variable)
-    data2 = desired_lines.replace(variable, updateversion)
-    print(data2)
-    print("Text replaced")
-    # lines = file.writelines(line.replace('variable', '7777777'))
+    existversion = desired_lines[42:48]
+    print(existversion)
+    data2 = desired_lines.replace(existversion, updateversion)
 else:
   print("invalid line")
 
-with open('example2.txt', 'r') as file:
-  print(variable)
-  print(updateversion)
+with open(release_history, 'r') as file:
   contents = file.read()
   contents = contents.replace(variable,updateversion)
 
-with open('example2.txt', "w") as file:
+with open(release_history, "w") as file:
   file.write(contents)
   file.close()
   
-  
+# use copyfile() 
+shutil.copyfile(release_template,release_notes)  
